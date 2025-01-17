584a78319b13414c464f03ad34231e41d7210224
David Koen <davidodawie@gmail.com>
2025-01-17 15:38:09 +0200

    pushed chapter 2

---
+++ chapter-2/hello_world.py
@@ -0,0 +1,27 @@
+message = "Hello, World!"
+
+print(message)
+
+message = "Changed text."
+
+print(message)
+
+########################################
+
+greeting = "Hello"
+
+name = "David"
+
+print(greeting + " " + name + ".")
+
+########################################
+
+print("Hello " * 3)
+
+#SLICING#################################
+
+print("Hello, World!"[7:12])
+
+#ESCAPE CHARACTERS#######################
+
+print("Line 1")
